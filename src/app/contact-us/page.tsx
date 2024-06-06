1aae88241d9650e989f9a8e97de5da1222438d0a
chamaxx9zp <chamakapiyumal@gmail.com>
2024-06-06 11:23:18 +0530

    add routes

---
+++ src/app/contact-us/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <>
+      <h1>Contact Us Page</h1>
+    </>
+  )
+}
+
+export default page
---
+++ src/app/terms-and-conditions/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <>
+      <h1>Terms And Conditions Page</h1>
+    </>
+  )
+}
+
+export default page
