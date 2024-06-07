8ca82b8bfbb482fe502cea8d3b11d859a9fbfa08
chamaxx9zp <chamakapiyumal@gmail.com>
2024-06-07 15:37:29 +0530

    add featured image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 import Image from "next/image";
 import styles from "./page.module.scss";
-import Carousel from 'react-bootstrap/Carousel';
-import CarouselImage from '../../public/homepage/hero-slider/adorable-dog-park-nature-with-owner.jpg';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Carousel from "react-bootstrap/Carousel";
+import CarouselImage from "../../public/homepage/hero-slider/adorable-dog-park-nature-with-owner.jpg";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+// featured component
+import Figure from "react-bootstrap/Figure";
+import feature1 from "../../public/homepage/featured-products/featured1.png";
 
 export default function Home() {
   return (
@@ -20,14 +24,36 @@ export default function Home() {
                   alt="carousel_image"
                   className="imgcont"
                 />
-                <Carousel.Caption>
-                  <h3>First slide label</h3>
-                  <p>
-                    Nulla vitae elit libero, a pharetra augue mollis interdum.
+                <Carousel.Caption className="hero-caption">
+                  <h1 className="text-start">Furry Friends Haven</h1>
+                  <p className="text-start">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
                   </p>
+                  <Button
+                    className="d-flex justify-content-start"
+                    variant="light"
+                  >
+                    Shop Now
+                  </Button>
                 </Carousel.Caption>
               </Carousel.Item>
             </Carousel>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          <Col md={3}>
+           
+              <Image
+                width={171}
+                height={180}
+                alt="171x180"
+                src={feature1}
+              />
+             
           </Col>
         </Row>
       </Container>
