8051e6026cdd27916243f05dea39973b5cae0072
chamaxx9zp <chamakapiyumal@gmail.com>
2024-06-07 09:30:15 +0530

    hero slider initialize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,36 @@
 "use client";
 import Image from "next/image";
 import styles from "./page.module.scss";
-import Accordion from 'react-bootstrap/Accordion';
-import Button from 'react-bootstrap/Button';
+import Carousel from 'react-bootstrap/Carousel';
+import CarouselImage from '../../public/homepage/hero-slider/adorable-dog-park-nature-with-owner.jpg';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
-      <Accordion defaultActiveKey="0">
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
-    <div>
-    <h1 className={styles.title}>hi there!</h1>
-        <nav className={styles.nav}>
-        <ul>
-            <li><a href="#">Home <span>hi</span> </a></li>
-            <li className={styles.about}><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-        </nav>
-        <Button variant="primary">Primary</Button>
-    </div>
+      <Container fluid className="px-0">
+        <Row>
+          <Col>
+            <Carousel indicators={false} controls={false} interval={null}>
+              <Carousel.Item>
+                <Image
+                  src={CarouselImage}
+                  alt="carousel_image"
+                  className="imgcont"
+                />
+                <Carousel.Caption>
+                  <h3>First slide label</h3>
+                  <p>
+                    Nulla vitae elit libero, a pharetra augue mollis interdum.
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
