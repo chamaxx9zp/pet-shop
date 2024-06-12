41b8a7ab3ab42553dc7f9e911577bd89ef0e4be8
chamaxx9zp <chamakapiyumal@gmail.com>
2024-06-12 21:09:34 +0530

    add products component to shop page

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSlider from '../components/shop-page/hero-slider/HeroSlider'
+import Products from '../components/shop-page/products/Products'
 
 function page() {
   return (
     <>
       <HeroSlider/>
+      <Products/>
     </>
   )
 }
