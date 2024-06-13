d04fce8ada87ce509a01558dc65580055e91f419
chamaxx9zp <chamakapiyumal@gmail.com>
2024-06-13 17:29:19 +0530

    add terms and condition page

---
+++ src/app/components/terms-conditions-page/terms-section/Terms.tsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import { Col,Row,Container  } from 'react-bootstrap';
+
+function Terms() {
+  return (
+    <>
+     <Container>
+      <Row className='py-5'>
+        <Col>
+        <h6 className="">Welcome to Our Pet Shop!</h6>
+        <br />
+        <p>These terms and conditions outline the rules and regulations for the use of Our Pet Shop Website.</p>
+        <p>By accessing this website, we assume you accept these terms and conditions. Do not continue to use Our Pet Shop if you do not
+agree to take all of the terms and conditions stated on this page.</p>
+        <br />
+        <h6 className="">Terminology:</h6>
+        <br />
+        <p>The following terminology applies to these Terms and Conditions, Privacy Statement, and Disclaimer Notice and all Agreements:</p>
+        <p>"Client," "You," and "Your" refers to you, the person logging on to this website and compliant with the Company's terms and conditions.
+"The Company," "Ourselves," "We," "Our," and "Us," refers to our Company.
+"Party," "Parties," or "Us," refers to both the Client and ourselves.
+Cookies:</p>
+        <p>We employ the use of cookies. By accessing Our Pet Shop, you agreed to use cookies in agreement with the Our Pet Shop's Privacy Policy.</p>
+        <br />
+        <h6 className="">License:</h6>
+        <br />
+        <p>Unless otherwise stated, Our Pet Shop and/or its licensors own the intellectual property rights for all material on Our Pet Shop. All
+intellectual property rights are reserved. You may access this from Our Pet Shop for your own personal use subjected to restrictions set in
+these terms and conditions.</p>
+        <br />
+        <h6 className="">You must not:</h6>
+        <br />
+        <p>Republish material from Our Pet Shop <br />
+Sell, rent, or sub-license material from Our Pet Shop <br />
+Reproduce, duplicate, or copy material from Our Pet Shop <br />
+Redistribute content from Our Pet Shop</p>
+        <br />
+        <h6 className="">User Comments:</h6>
+        <br />
+        <p>This Agreement shall begin on the date hereof.
+Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. Our Pet
+Shop does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and
+opinions of Our Pet Shop, its agents, or affiliates. Comments reflect the views and opinions of the person who posts their views
+and opinions. To the extent permitted by applicable laws, Our Pet Shop shall not be liable for the Comments or for any liability,
+damages, or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this
+website.</p>
+        <br />
+        <h6 className="">Content Liability:</h6>
+        <br />
+        <p>We shall not be held responsible for any content that appears on your Website. You agree to protect and defend us against all claims that
+are rising on your Website. No link(s) should appear on any Website that may be interpreted as libelous, obscene, or criminal, or which
+infringes, otherwise violates, or advocates the infringement or other violation of, any third-party rights.</p>
+        <br />
+        <h6 className="">Reservation of Rights:</h6>
+        <br />
+        <p>We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links
+to our Website upon request. We also reserve the right to amen these terms and conditions and it's linking policy at any time. By
+continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.</p>
+        <br />
+        <h6 className="">Removal of links from our website:</h6>
+        <br />
+        <p>If you find any link on our Website that is offensive for any reason, you are free to contact and inform us at any moment. We will consider
+requests to remove links but we are not obligated to or so or to respond to you directly.</p>
+        <br />
+        <h6 className="">Disclaimer:</h6>
+        <br />
+        <p>Limit or exclude our or your liability for death or personal injury; <br />
+Limit or exclude our or your liability for fraud or fraudulent misrepresentation; <br />
+Limit any of our or your liabilities in any way that is not permitted under applicable law; or <br />
+Exclude any of our or your liabilities that may not be excluded under applicable law. <br />
+The limitations and prohibitions of liability set in this Section and elsewhere in this disclaimer: (a) are subject to the preceding paragraph;
+and (b) govern all liabilities arising under the disclaimer, including liabilities arising in contract, in tort, and for breach of statutory duty.</p>
+        <br />
+        <p>As long as the website and the information and services on the website are provided free of charge, we will not be liable for any loss or
+damage of any nature.</p>
+        </Col>
+      </Row>
+    </Container>
+    </>
+  )
+}
+
+export default Terms
--- src/app/terms-and-conditions/page.tsx
+++ src/app/terms-and-conditions/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSlider from '../components/terms-conditions-page/hero-slider/HeroSlider'
+import Terms from '../components/terms-conditions-page/terms-section/Terms'
 
 function page() {
   return (
     <>
     <HeroSlider/>
+    <Terms/>
     </>
   )
 }
