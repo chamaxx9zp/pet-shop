61865f966f400ccdfb7053270bd0cfed1331a92f
chamaxx9zp <chamakapiyumal@gmail.com>
2024-06-13 15:15:36 +0530

    add form component to contact us page

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSlider from '../components/contact-us-page/hero-slider/HeroSlider'
+import FormSection from '../components/contact-us-page/form-section/FormSection'
 
 function page() {
   return (
     <>
     <HeroSlider/>
+    <FormSection/>
     </>
   )
 }
